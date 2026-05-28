07d8dc83eb6e354b0c6ef14d0740764082afa2da
bread-pie <x604059@gmail.com>
2026-05-28 11:18:30 +0800

    birthday website

--- cause.js
+++ cause.js
@@ -1,22 +1,22 @@
  // Reasons database
  const reasons = [
     { 
-        text: "You’re such a kind and wonderful person, and I feel lucky to share such a good bond with you. 💖", 
+        text: "You’re such a kind and wonderful person, and I feel lucky to share such a good bond with you. 🫂💖", 
         emoji: "🌟",
         gif: "gif1.gif"
     },
     { 
-        text: "May your day be filled with love, laughter, and endless joy. 🌸 ", 
+        text: "May your day be filled with love, laughter, and endless joy. 🤞🌸 ", 
         emoji: "💗",
         gif: "gif2.gif"
     },
     { 
-        text: "Wishing you success, happiness, and everything your heart desires. ✨ ", 
+        text: "Wishing you success, happiness, and everything your heart desires. 🙏✨ ", 
         emoji: "💕",
         gif: "gif1.gif"
     },
     { 
-        text: "Stay the amazing girl you are—always spreading positivity around. Have the happiest year ahead! 🥳 ", 
+        text: "I love you so much. Have the happiest year ahead! 🥰🥳 ", 
         emoji: "🌟",
         gif: "gif2.gif"
     }
